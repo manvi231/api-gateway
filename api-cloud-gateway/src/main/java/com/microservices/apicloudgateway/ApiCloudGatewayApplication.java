aace3a52ea36061e99155fc98669c0ef02cc7c9b
manvi231 <99906463+manvi231@users.noreply.github.com>
2022-02-17 23:50:36 +0530

    Add files via upload

---
+++ api-cloud-gateway/src/main/java/com/microservices/apicloudgateway/ApiCloudGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.microservices.apicloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCloudGatewayApplication.class, args);
+	}
+
+}
